fb325772e47191d12b5e431548b48f810e4a0bc5
tamtom <omar.tamimid1@gmail.com>
2023-06-24 12:12:33 +0400

    upgreade dependecies, solution excersice1

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
@@ -14,9 +14,13 @@ import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Exercise1Fragment : BaseFragment() {
-
+    private val coroutinesScope = CoroutineScope(Dispatchers.Main)
     override val screenTitle get() = ScreenReachableFromHome.EXERCISE_1.description
 
     private lateinit var edtUserId: EditText
@@ -34,7 +38,7 @@ class Exercise1Fragment : BaseFragment() {
 
         edtUserId = view.findViewById(R.id.edt_user_id)
         edtUserId.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 btnGetReputation.isEnabled = !s.isNullOrEmpty()
@@ -46,20 +50,25 @@ class Exercise1Fragment : BaseFragment() {
         btnGetReputation = view.findViewById(R.id.btn_get_reputation)
         btnGetReputation.setOnClickListener {
             logThreadInfo("button callback")
-            btnGetReputation.isEnabled = false
-            getReputationForUser(edtUserId.text.toString())
-            btnGetReputation.isEnabled = true
+            coroutinesScope.launch {
+                btnGetReputation.isEnabled = false
+                val reputation = getReputationForUser(edtUserId.text.toString())
+                btnGetReputation.isEnabled = true
+                Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         return view
     }
 
-    private fun getReputationForUser(userId: String) {
+    private suspend fun getReputationForUser(userId: String): Int {
         logThreadInfo("getReputationForUser()")
-
-        val reputation = getReputationEndpoint.getReputation(userId)
-
-        Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
+        val reputation = withContext(Dispatchers.IO) {
+            val reputation = getReputationEndpoint.getReputation(userId)
+            reputation
+        }
+        return reputation
     }
 
     private fun logThreadInfo(message: String) {
--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
@@ -4,7 +4,7 @@ import com.techyourchance.coroutines.common.ThreadInfoLogger
 import kotlin.random.Random
 
 class GetReputationEndpoint {
-    fun getReputation(userId: String): Int {
+     fun  getReputation(userId: String): Int {
         ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): called")
         Thread.sleep(3000)
         ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): return data")
